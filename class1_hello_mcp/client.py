c45d94bcffa487365b0438895c40a9d4011b2edb
Umm e Habiba <161445850+UmmeHabiba1312@users.noreply.github.com>
2025-08-12 17:28:02 +0500

    Update client.py

--- class1_hello_mcp/client.py
+++ class1_hello_mcp/client.py
@@ -1,11 +1,12 @@
 import requests
 
-url = "http://localhost:9000/mcp/"
+url = "http://localhost:8000/mcp/"
 
 headers = {
     "Accept": "application/json,text/event-stream",
 }
 
+# jsonrpc stands for JSON Remote Procedure Call.
 body = {
     "jsonrpc": "2.0",
     "method": "tools/list",
@@ -19,3 +20,16 @@ print(response.text)
 for lines in response.iter_lines():
     if lines:
         print(lines)
+
+# tool calling testing with python client.py
+# {
+#     "jsonrpc": "2.0",
+#     "method": "tools/call",
+#     "params": {
+#         "name":"get_weather",
+#         "arguments":{
+#             "location":"Karachi"
+#         }
+#     },
+#     "id": 1
+# }
